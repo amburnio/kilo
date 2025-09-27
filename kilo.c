2145a5cd73b4657e45f0dacb0de2864191f271b2
Zach Amburn <64771834+amburnio@users.noreply.github.com>
2025-09-26 23:20:52 -0500

    Set q to exit program and turn off key echo
    Key echo is turned off so that typed keys aren't printed in the
    terminal. This is important for setting the terminal to raw mode.

--- kilo.c
+++ kilo.c
@@ -1,3 +1,30 @@
+#include "kilo.h"
+
+struct termios orig_termios;
+
 int main(void) {
+    enableRawMode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
+}
+
+void enableRawMode(void) {
+    atexit(disableRawMode);
+
+    // Get current terminal attributes
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    struct termios raw = orig_termios;
+
+    // Turn off echo attribute
+    raw.c_lflag &= ~(ECHO);
+
+    // Set terminal attributes
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+void disableRawMode(void) {
+    // Set terminal attributes to original state
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
---
+++ kilo.h
@@ -0,0 +1,8 @@
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+// Prototypes
+
+void enableRawMode(void);
+void disableRawMode(void);
